729b832ba52730752e2a02282e4b391accbfae60
Alex Donyush <donyush@list.ru>
2020-03-29 04:09:05 +0300

    parsing done

--- index.php
+++ index.php
@@ -6,10 +6,96 @@ error_reporting(E_ALL);
  require_once (__DIR__."\\php\\readxl.php");
  require_once (__DIR__."\\php\\settings.php");
 
- $ras = new Excel("ex.xlsx", "\\..\\excel\\");
- $ras->setSheet(1);
- $ras->readCol(2,28,10);
- //var_dump($ras->worksheet->toArray(null, true, true, true));
+ $plan = new Excel("ex.xlsx", "\\..\\excel\\");
+ $plan->setSheet(1);
+
+ // $one=$plan->readRow(10,[2,3,4,5,6]);
+ // var_dump($one);
+$itog = array();
+$startRow=10;
+$i=10;
+$readColsArray=[2,3,4,5,6];
+$colsNotNull=[2,3,4,5,6];
+$numRows=$plan->worksheet->getHighestRow() ;//+ 100;
+// for ($i=$startRow; $i<$numRows; $i++)
+// {
+do {
+    $row=$plan->readRow($i,$readColsArray);
+    $allColsNull=false;
+    foreach ($colsNotNull as $value) {
+      if ($row["$value"] != null)
+      {
+        break;
+      }
+      $allColsNull=true;
+    }
+
+    if ($plan->last_data[2]->getStyle()->getFont()->getItalic()== true)
+    {
+      $allColsNull=true;
+    }
+    if($allColsNull == false)
+    {
+      $itog["$i"]=$row;
+    }
+    $allColsNull=false;
+    $i++;
+    //print_r($plan->last_data[2]->getStyle()->getBorders()->getRight()->getBorderStyle());
+    //print_r($plan->last_data[2]->getStyle()->getBorders()->getRight()->getBorderStyle());
+} while ($i<=$numRows);//$plan->last_data[2]->getStyle()->getBorders()->getRight()->getBorderStyle() == "medium");
+//var_dump($itog);
+//print_r($plan->last_data[2]->getStyle()->getBorders()->getRight());
+//$plan->last_data[2]->getStyle()->getBorders()->getRight() !=null
+///////////////////////////////////////////
+foreach ($itog as $key => $value) {
+  var_dump($value);
+  echo "<br>";
+}
+
+///////////////////////////////
+ // $name=$plan->readCol(2,10);
+ // $exams=$plan->readCol(3,10);
+ // $zach=$plan->readCol(4,10);
+ // $kp=$plan->readCol(5,10);
+ // $kr=$plan->readCol(6,10);
+ //
+ //
+ // foreach ($name as $value) {
+ //
+ //   echo $value . "<br>";
+ //   // $itog["$i"]['name']=$value->getCalculatedValue();
+ //   // $itog["$i"]['exams']=$exams["$i"]->getCalculatedValue();
+ //   // $itog["$i"]['zach']=$zach["$i"]->getCalculatedValue();
+ //   // $itog["$i"]['kp']=$kp["$i"]->getCalculatedValue();
+ //   // $itog["$i"]['kr']=$kr["$i"]->getCalculatedValue();
+ //   // $i++;
+ // }
+ // var_dump($name);
+ // var_dump($exams);
+ // var_dump($zach);
+ // var_dump($kp);
+ // var_dump($kr);
+ // $i=0;
+ // foreach ($name as $value) {
+ //
+ //   $itog["$i"]['name']=$value->getCalculatedValue();
+ //   $itog["$i"]['exams']=$exams["$i"]->getCalculatedValue();
+ //   $itog["$i"]['zach']=$zach["$i"]->getCalculatedValue();
+ //   $itog["$i"]['kp']=$kp["$i"]->getCalculatedValue();
+ //   $itog["$i"]['kr']=$kr["$i"]->getCalculatedValue();
+ //   $i++;
+ // }
+//  for ($i=0; $i<count($name);$i++)
+//  {
+//    $itog["$i"]['name']=$value->getCalculatedValue();
+//     $itog["$i"]['exams']=$exams["$i"]->getCalculatedValue();
+//     $itog["$i"]['zach']=$zach["$i"]->getCalculatedValue();
+//     $itog["$i"]['kp']=$kp["$i"]->getCalculatedValue();
+//     $itog["$i"]['kr']=$kr["$i"]->getCalculatedValue();
+//  }
+// var_dump($itog);
+
+ //var_dump($plan->worksheet->toArray(null, true, true, true));
  // use PhpOffice\PhpSpreadsheet\IOFactory;
  //
  // require 'C:\Users\UserPC12\Desktop\Al\WEB\OSPanel\domains\IT-kursach2\vendor\phpoffice\phpspreadsheet\samples\Header.php';
--- php/readxl.php
+++ php/readxl.php
@@ -1,21 +1,9 @@
 <?php
-//require_once "settings.php";
 
-// $settings = array(
-//   "path_to_libs"=>__DIR__ . "\\..\\libs\\vendor\\",
-//   "path_to_autoloader"=> __DIR__ . "\\..\\libs\\vendor\\autoload.php",
-//   "path_to_excel"=>__DIR__ . "\\..\\excel\\",
-// );
-
-//require_once $settings["path_to_autoloader"];
  require_once(__DIR__. '\\..\\vendor\\autoload.php');
  use PhpOffice\PhpSpreadsheet\IOFactory;
-// use PhpOffice\PhpSpreadsheet\Spreadsheet;
-// use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-/**
- *
- */
-class Excel //extends AnotherClass
+ use \PhpOffice\PhpSpreadsheet\Style\Font;
+class Excel
 {
   public $path_to_excel;
   public $excel;
@@ -47,31 +35,18 @@ class Excel //extends AnotherClass
     $this->spreadsheet->setActiveSheetIndex($num);
     $this->worksheet = $this->spreadsheet->getActiveSheet();//->toArray(null, true, true, true);
     var_dump("setSheet() complete");
-    //var_dump($this->worksheet->toArray(null, true, true, true));
-    // if (empty($this->excel))
-    // {
-    //   return 1;
-    // }
-    //$this->spreadsheet->setActiveSheetIndex(1);
-    //$this->last_data =
-  //  var_dump($this->spreadsheet->getActiveSheet()->toArray(null, true, true, true));
-  //  var_dump($this->last_data);
-    //var_dump($this->worksheet->toArray(null, true, true, true));
-    //$this->worksheet = $this->spreadsheet->getActiveSheet();
-    // var_dump($this->spreadsheet->getActiveSheet()->toArray(null, true, true, true));
-    // var_dump($this->worksheet->toArray(null, true, true, true));
-    //$this->spreadsheet = getSheet($num);
   }
 
 
-  public function readCol($col, $stop=-1, $start=1, $callbackFilter=-1)
+  public function readCol(int $col, int $start=1, int $stop=null, callable $callbackFilter=null)
   {
+    $this->last_data=[];
     if (empty($this->worksheet))
     {
       echo "Worksheet is empty <br>";
       return;
     }
-    if ($stop==-1)
+    if ($stop==null)
     {
       $stop=$this->worksheet->getHighestRow();
     }
@@ -84,71 +59,76 @@ class Excel //extends AnotherClass
     {
       if (is_callable($callbackFilter))
       {
-        $style=$this->worksheet->getStyle($col, $i);
-        if ($callbackFilter($style))
+        $cell=$this->worksheet->getCellByColumnAndRow($col, $i);
+        if ($callbackFilter($cell))
         {
             //echo "callback is callable <br>";
           $retVal[$i] = $this->worksheet->getCellByColumnAndRow($col, $i)->getCalculatedValue();
+          $this->last_data[$i]=$cell;
         }
       }
       else {
       //echo "callback is not callable <br>";
-      $retVal[$i] = $this->worksheet->getCellByColumnAndRow($col, $i)->getCalculatedValue();
+      $cell=$this->worksheet->getCellByColumnAndRow($col, $i);
+      $retVal[$i] = $cell->getCalculatedValue();
+      $this->last_data[$i]=$cell;
       }
 
     }
-    $this->last_data = $retVal;
-    var_dump($this->last_data);
+    // $this->last_data = $retVal;
+    // var_dump($this->last_data);
     return $retVal;
   }
-
-
-//   public function getCellValue($cellOrCol, $row = null, $format = 'd.m.Y')
-//     {
-//         //column set by index
-//         // if(is_numeric($cellOrCol)) {
-//         //     $cell = $this->worksheet->getCellByColumnAndRow($cellOrCol, $row);
-//         // } else {
-//         //     $lastChar = substr($cellOrCol, -1, 1);
-//         //     if(!is_numeric($lastChar)) { //column contains only letter, e.g. "A"
-//         //        $cellOrCol .= $row;
-//         //     }
-//         //
-//         //     $cell = $this->worksheet->getCell($cellOrCol);
-//         // }
-//
-//         //try to find current coordinate in all merged cells ranges
-//         //if find -> get value from head cell
-//         // foreach($this->mergedCellsRange as $currMergedRange){
-//         //     if($cell->isInRange($currMergedRange)) {
-//         //         $currMergedCellsArray = PHPExcel_Cell::splitRange($currMergedRange);
-//         //         $cell = $this->worksheet->getCell($currMergedCellsArray[0][0]);
-//         //         break;
-//         //     }
-//         // }
-//
-//         //simple value
-//         // $val = $cell->getValue();
-//         //
-//         // //date
-//         // if(PHPExcel_Shared_Date::isDateTime($cell)) {
-//         //      $val = date($format, PHPExcel_Shared_Date::ExcelToPHP($val));
-//         // }
-//         //
-//         // //for incorrect formulas take old value
-//         // if((substr($val,0,1) === '=' ) && (strlen($val) > 1)){
-//         //     $val = $cell->getOldCalculatedValue();
-//         // }
-//         //
-//         // return $val;
-//     }
- }
-// echo PHP_EOL . $settings["path_to_excel"] . '<br>';
-// echo PHP_EOL . $settings["path_to_libs"] . '<br>';
-// echo PHP_EOL . $settings["path_to_autoloader"] . '<br>';
-// echo PHP_EOL . $settings["path_to_excel"] . $filename;   $settings["path_to_excel"], $filename
-
-
-
-
+  public function readRow(int $row, iterable $cols=null, callable $callbackFilter=null)
+  {
+    $this->last_data=[];
+    if (empty($this->worksheet))
+    {
+      echo "Worksheet is empty <br>";
+      return;
+    }
+    if ($cols == null)
+    {
+      $colLen =\PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($this->worksheet->getHighestColumn());
+      for ($i=0; $i<=$colLen; $i++)
+      {
+        if ($callbackFilter != null)
+        {
+          $cell= $this->worksheet->getCellByColumnAndRow($i,$row);
+          if ($callbackFilter($cell))
+          {
+              //echo "callback is callable <br>";
+            $retVal[$i] = $cell->getCalculatedValue();
+            $this->last_data[$i]=$cell;
+          }
+        }
+        else {
+            $cell=$this->worksheet->getCellByColumnAndRow($i,$row);
+            $retVal[$i] = $cell->getCalculatedValue();
+            $this->last_data[$i]=$cell;
+        }
+      }
+    }
+    else {
+      foreach ($cols as $key => $value)
+      {
+        if ($callbackFilter != null)
+        {
+          $cell= $this->worksheet->getCellByColumnAndRow($value, $row);
+          if ($callbackFilter($cell))
+          {
+          $retVal[$value] =  $cell->getCalculatedValue();
+          $this->last_data[$value]=$cell;
+          }
+        }
+        else {
+          $cell= $this->worksheet->getCellByColumnAndRow($value, $row);
+          $retVal[$value] =  $cell->getCalculatedValue();
+          $this->last_data[$value]=$cell;
+        }
+      }
+    }
+    return $retVal;
+  }
+}
  ?>
